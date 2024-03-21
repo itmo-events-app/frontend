0e25c564174a04cfa99fe537d73882d56857a62c
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-03-21 14:39:25 +0300

    feat: add button component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import AppRouter from "./router/AppRouter";
+import "./App.css";
 
 const App = () => {
   return (
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ReactLogo, ViteLogo } from "@shared/ui/icons/index";
-import "./Root.css";
+import styles from "./Root.module.css";
+import ButtonAuth from "@widgets/ButtonAuth";
 
 function Root() {
   const [count, setCount] = useState(0);
@@ -8,21 +9,8 @@ function Root() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <ViteLogo className="logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <ReactLogo className="logo react" />
-        </a>
+        <ButtonAuth className={styles.button}>Войти</ButtonAuth>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMRd asfas f
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
