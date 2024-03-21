54184005b93b55ea7f30b4c2af8b52d83ee3888b
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-03-21 17:54:19 +0300

    feat: add login page, update components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,16 +4,41 @@ import Input from "@widgets/auth/Input";
 
 import styles from './index.module.css';
 import Label from "@widgets/auth/InputLabel";
+import Link from "@widgets/auth/Link";
+import Error from "@widgets/auth/Error";
+import { useState } from "react";
 
 
 function Root() {
+  const [isError, setIsError] = useState(true);
+
+  const _forgotPassword = () => {
+    console.log('forgot password!');
+  }
+
+  const _register = () => {
+    console.log('register!');
+  }
+
   return (
     <>
       <div>
         <Block className={styles.block}>
-          <Label value="Hello"/>
-          <Input/>
+          <span className={styles.header}>Войти</span>
+          <Error value="Неправильный логин или пароль" isError={isError}/>
+          <div className={styles.form}>
+            <div className={styles.form_item}>
+              <Label value="Логин или номер ИСУ"/>
+              <Input/>
+            </div>
+            <div className={styles.form_item}>
+              <Label value="Пароль"/>
+              <Input type='password'/>
+              <Link onClick={_forgotPassword} value="Забыли пароль?"/>
+            </div>
+          </div>
           <Button>Войти</Button>
+          <Link className={styles.register} onClick={_register} value="Нет учетной записи? Зарегистрироваться" />
         </Block>
       </div>
     </>
---
+++ src/widgets/auth/Error/index.tsx
@@ -0,0 +1,19 @@
+import styles from './index.module.css';
+
+type Props = {
+  className?: string,
+  value: string,
+  isError: boolean,
+}
+
+function Error(props: Props) {
+  return <>
+    {
+      props.isError 
+        ? <span className={styles.error}>{props.value}</span>
+        : <></>
+    }
+    </>
+}
+
+export default Error;
