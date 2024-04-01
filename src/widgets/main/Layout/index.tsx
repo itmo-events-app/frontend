1182d5ddf798764722a277b7db3e1836095a5b7e
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-01 15:54:57 +0300

    fix: layout auto height heading using js, add scrollable content

--- src/widgets/main/Layout/index.tsx
+++ src/widgets/main/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styles from './index.module.css'
 
 type Props = {
@@ -9,23 +10,44 @@ type Props = {
 }
 
 function Layout(props: Props) {
+  const topLeftRef = useRef(null);
+  const topRightRef = useRef(null);
+
+  // get maximum height of left and right components, apply maximum
+  useEffect(() => {
+    let height = 0;
+    if (topLeftRef.current) {
+      height = (topLeftRef.current as any).offsetHeight;
+    }
+    if (topRightRef.current) {
+      let h2 = (topRightRef.current as any).offsetHeight;
+      height = h2 > height ? h2 : height;
+    }
+    (topLeftRef.current as any).style.height = `${height}px`;
+    (topRightRef.current as any).style.height = `${height}px`;
+  })
+
   return (
     <div className={styles.root}>
-      <div className={styles.grid}>
-        <div className={styles.grid_one}>
-          {props.topLeft}
+      <div className={styles.columns}>
+        <div className={styles.column_left}>
+          <div className={styles.top_left} ref={topLeftRef}>
+            {props.topLeft}
+          </div>
+          <div className={styles.bottom_left}>
+            {props.bottomLeft}
+          </div>
         </div>
-        <div className={styles.grid_two}>
-          {props.topRight}
-        </div>
-        <div className={styles.grid_three}>
-          {props.bottomLeft}
-        </div>
-        <div className={styles.grid_four}>
-          {props.bottomRight}
+        <div className={styles.column_right}>
+          <div className={styles.top_right} ref={topRightRef}>
+            {props.topRight}
+          </div>
+          <div className={styles.bottom_right}>
+            {props.bottomRight}
+          </div>
         </div>
       </div>
-      {props.children} 
+      {props.children}
     </div>
   )
 }
