0c2e8f1db8b89815297b7bba753ba13c81bc6098
Max <max.kuzn.msx@gmail.com>
2024-04-02 22:01:43 +0300

    fix links from events page

--- src/pages/main/AvailableEvents/index.tsx
+++ src/pages/main/AvailableEvents/index.tsx
@@ -11,63 +11,17 @@ import Button from "@widgets/main/Button";
 import PagedList, { PageEntry } from "@widgets/main/PagedList";
 import { RoutePaths } from '@shared/config/routes';
 import Input from "@widgets/main/Input";
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const _displayModes: DropdownOption[] = [
   new DropdownOption("Показать списком"),
   new DropdownOption("Показать на карте")
 ]
 
-function _entryStub(index: number) {
-  return (
-    <div key={index} className={styles.event_entry}>
-      <ReactLogo className={styles.event_icon} />
-      <div className={styles.event_info_column}>
-        <div className={styles.event_name}>
-          {"Event " + index}
-        </div>
-        <div className={styles.event_place}>
-          Place
-        </div>
-      </div>
-    </div>
-  );
-}
 
-const _events: any[] = [
-  new PageEntry(() => { return _entryStub(1) }),
-  new PageEntry(() => { return _entryStub(2) }),
-  new PageEntry(() => { return _entryStub(3) }),
-  new PageEntry(() => { return _entryStub(4) }),
-  new PageEntry(() => { return _entryStub(5) }),
-  new PageEntry(() => { return _entryStub(6) }),
-  new PageEntry(() => { return _entryStub(7) }),
-  new PageEntry(() => { return _entryStub(8) }),
-  new PageEntry(() => { return _entryStub(9) }),
-  new PageEntry(() => { return _entryStub(10) }),
-  new PageEntry(() => { return _entryStub(11) }),
-  new PageEntry(() => { return _entryStub(12) }),
-  new PageEntry(() => { return _entryStub(13) }),
-  new PageEntry(() => { return _entryStub(14) }),
-  new PageEntry(() => { return _entryStub(15) }),
-  new PageEntry(() => { return _entryStub(16) }),
-  new PageEntry(() => { return _entryStub(17) }),
-  new PageEntry(() => { return _entryStub(18) }),
-  new PageEntry(() => { return _entryStub(19) }),
-  new PageEntry(() => { return _entryStub(20) }),
-  new PageEntry(() => { return _entryStub(21) }),
-  new PageEntry(() => { return _entryStub(22) }),
-  new PageEntry(() => { return _entryStub(23) }),
-  new PageEntry(() => { return _entryStub(24) }),
-  new PageEntry(() => { return _entryStub(25) }),
-  new PageEntry(() => { return _entryStub(26) }),
-  new PageEntry(() => { return _entryStub(27) }),
-  new PageEntry(() => { return _entryStub(28) }),
-  new PageEntry(() => { return _entryStub(29) }),
-  new PageEntry(() => { return _entryStub(30) }),
-  new PageEntry(() => { return _entryStub(31) }),
-  new PageEntry(() => { return _entryStub(32) }),
-  new PageEntry(() => { return _entryStub(33) })
-]
+
+
 
 const filterStatus: DropdownOption[] = [
   new DropdownOption("Активное"),
@@ -99,7 +53,61 @@ function AvailableEventsPage() {
   const _onCreation = () => {
     console.log('creating')
   }
+  const navigate = useNavigate();
+  const _event = () => {
+    navigate(RoutePaths.eventData);
+  }
+  const _events: any[] = [
+    new PageEntry(() => { return _entryStub(1) }),
+    new PageEntry(() => { return _entryStub(2) }),
+    new PageEntry(() => { return _entryStub(3) }),
+    new PageEntry(() => { return _entryStub(4) }),
+    new PageEntry(() => { return _entryStub(5) }),
+    new PageEntry(() => { return _entryStub(6) }),
+    new PageEntry(() => { return _entryStub(7) }),
+    new PageEntry(() => { return _entryStub(8) }),
+    new PageEntry(() => { return _entryStub(9) }),
+    new PageEntry(() => { return _entryStub(10) }),
+    new PageEntry(() => { return _entryStub(11) }),
+    new PageEntry(() => { return _entryStub(12) }),
+    new PageEntry(() => { return _entryStub(13) }),
+    new PageEntry(() => { return _entryStub(14) }),
+    new PageEntry(() => { return _entryStub(15) }),
+    new PageEntry(() => { return _entryStub(16) }),
+    new PageEntry(() => { return _entryStub(17) }),
+    new PageEntry(() => { return _entryStub(18) }),
+    new PageEntry(() => { return _entryStub(19) }),
+    new PageEntry(() => { return _entryStub(20) }),
+    new PageEntry(() => { return _entryStub(21) }),
+    new PageEntry(() => { return _entryStub(22) }),
+    new PageEntry(() => { return _entryStub(23) }),
+    new PageEntry(() => { return _entryStub(24) }),
+    new PageEntry(() => { return _entryStub(25) }),
+    new PageEntry(() => { return _entryStub(26) }),
+    new PageEntry(() => { return _entryStub(27) }),
+    new PageEntry(() => { return _entryStub(28) }),
+    new PageEntry(() => { return _entryStub(29) }),
+    new PageEntry(() => { return _entryStub(30) }),
+    new PageEntry(() => { return _entryStub(31) }),
+    new PageEntry(() => { return _entryStub(32) }),
+    new PageEntry(() => { return _entryStub(33) })
+  ]
 
+  function _entryStub(index: number) {
+    return (
+      <a key={index} onClick={_event} className={styles.event_entry}>
+        <ReactLogo className={styles.event_icon} />
+        <div className={styles.event_info_column}>
+          <div className={styles.event_name}>
+            {"Event " + index}
+          </div>
+          <div className={styles.event_place}>
+            Place
+          </div>
+        </div>
+      </a>
+    );
+  }
   return (
     <Layout
       topLeft={<BrandLogo onClick={_brandLogoClick} />}
