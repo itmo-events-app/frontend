7fdaedc1ccdef60626394a9efe9e776197073b0f
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-10 21:28:16 +0300

    remove privilegeContext from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import AppRouter from "./AppRouter";
 import "./App.css";
-import { PrivilegeContextProvider } from "@features/PrivilegeProvider";
-import { userAdministrator } from "./privileges";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PrivilegeContextProvider from "@widgets/PrivilegeProvider";
 
 const App = () => {
-  const privilegeContext = userAdministrator;
   const queryClient = new QueryClient();
 
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <PrivilegeContextProvider context={privilegeContext}>
+        <PrivilegeContextProvider>
           <AppRouter />
         </PrivilegeContextProvider>
       </QueryClientProvider>
