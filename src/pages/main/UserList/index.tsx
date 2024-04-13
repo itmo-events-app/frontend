d273da79b0a63749537714a6c1546f7ef1dbea4e
Alexander Kuznecov <58745426+Darklol@users.noreply.github.com>
2024-04-13 15:27:07 +0500

    User list page (#38)
    * feat: user list new design
    
    * feat: role list radio widget
    
    * feat: assign role dialog
    
    * feat: user list working
    
    * accident EventData changes
    
    * feat: user list page is ready
    
    * feat: user list page is ready (styles)
    
    * feat: generated API

--- src/pages/main/UserList/index.tsx
+++ src/pages/main/UserList/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useContext, useEffect, useRef, useState} from 'react';
 import styles from './index.module.css';
 import BrandLogo from "@widgets/main/BrandLogo";
 import PageName from "@widgets/main/PageName";
@@ -8,21 +8,54 @@ import Content from "@widgets/main/Content";
 import PagedList, { PageEntry } from "@widgets/main/PagedList";
 import Layout from "@widgets/main/Layout";
 import { uid } from "uid";
-import Button from "@widgets/main/Button";
 import { appendClassName } from "@shared/util.ts";
-import { ArrowDown } from "@shared/ui/icons";
 import Search from "@widgets/main/Search";
 import Dialog from "@widgets/main/Dialog";
 import AssignDialogContent from "@pages/main/UserList/AssignDialogContent";
 import Fade from "@widgets/main/Fade";
+import ApiContext from "@features/api-context";
+import {toUserModel, UserModel} from "@entities/user";
+import {PrivilegeData} from "@entities/privilege-context";
+import {PrivilegeNames} from "@shared/config/privileges";
+import ContextMenu, {ContextMenuItem} from "@widgets/main/ContextMenu";
+import {hasAnyPrivilege} from "@features/privileges";
+import PrivilegeContext from "@features/privilege-context";
+import {MenuVertical} from "@shared/ui/icons";
+import RevokeDialogContent from "@pages/main/UserList/RevokeDialogContent";
+import MessageDialogContent from "@pages/main/UserList/MessageDialogContent";
+
+class ContextMenuData {
+  clientX: number;
+  clientY: number;
+  visible: boolean;
+  items: ContextMenuItem[];
+  constructor(
+    clientX: number = 0,
+    clientY: number = 0,
+    visible: boolean = false,
+    items: ContextMenuItem[] = []
+  ) {
+    this.clientX = clientX;
+    this.clientY = clientY;
+    this.visible = visible;
+    this.items = items;
+  }
+}
+
+enum DialogSelected {
+  NONE,
+  ASSIGN,
+  REVOKE,
+  MESSAGE
+}
 
 class DialogData {
   heading: string | undefined;
-  visible: boolean;
+  visible: DialogSelected;
   args: any;
   constructor(
     heading?: string,
-    visible: boolean = false,
+    visible: DialogSelected = DialogSelected.NONE,
     args: any = {}
   ) {
     this.heading = heading;
@@ -31,138 +64,151 @@ class DialogData {
   }
 }
 
-export class UserRole {
-  name: string;
-  description: string;
 
-  constructor(name: string, description: string) {
-    this.name = name;
-    this.description = description;
-  }
-}
+const privilegeOthers = {
+  assign: new Set([
+    new PrivilegeData(PrivilegeNames.ASSIGN_SYSTEM_ROLE)
+  ]),
+  revoke: new Set([
+    new PrivilegeData(PrivilegeNames.REVOKE_SYSTEM_ROLE)
+  ])
 
-class User {
-  id: string;
-  name: string;
-  surname: string;
-  email: string;
-  roles: UserRole[];
-
-  constructor(name: string, surname: string, email: string, roles: UserRole[]) {
-    this.id = uid();
-    this.name = name;
-    this.surname = surname;
-    this.email = email;
-    this.roles = roles;
-  }
-}
-
-const _userRoles: UserRole[] = [
-  new UserRole("ADMIN", "администратор"),
-  new UserRole("READER", "читатель"),
-  new UserRole("REDACTOR", "редактор"),
-]
-
-const _users: User[] = [
-  new User("Иван", "Иванов", "ivan@itmo.ru", _userRoles),
-  new User("Сергей", "Сергеев", "sergey@itmo.ru", _userRoles),
-  new User("Илья", "Ильин", "ilya@itmo.ru", _userRoles),
-  new User("Иван", "Иванов", "ivan@itmo.ru", _userRoles),
-  new User("Сергей", "Сергеев", "sergey@itmo.ru", _userRoles),
-  new User("Илья", "Ильин", "ilya@itmo.ru", _userRoles),
-  new User("Иван", "Иванов", "ivan@itmo.ru", _userRoles),
-  new User("Сергей", "Сергеев", "sergey@itmo.ru", _userRoles),
-  new User("Илья", "Ильин", "ilya@itmo.ru", _userRoles),
-  new User("Иван", "Иванов", "ivan@itmo.ru", _userRoles),
-  new User("Сергей", "Сергеев", "sergey@itmo.ru", _userRoles),
-  new User("Илья", "Ильин", "ilya@itmo.ru", _userRoles),
-  new User("Иван", "Иванов", "ivan@itmo.ru", _userRoles),
-  new User("Сергей", "Сергеев", "sergey@itmo.ru", _userRoles),
-  new User("Илья", "Ильин", "ilya@itmo.ru", _userRoles)
-];
-
-class UserEntry {
-  data: User;
-  expanded: boolean;
-
-  constructor(data: User, expanded: boolean) {
-    this.data = data;
-    this.expanded = expanded;
-  }
 }
 
 export default function UserListPage() {
-  const [users, setUsers] = useState(_users);
+  const { api } = useContext(ApiContext);
+  const { privilegeContext } = useContext(PrivilegeContext);
+  const [users, setUsers] = useState([] as UserModel[]);
+
+  const cmRef = useRef(null);
+  const dialogRef = useRef(null);
+
+  const [cmData, setCmData] = useState(new ContextMenuData());
   const [dialogData, setDialogData] = useState(new DialogData());
-  const [userEntries, setUserEntries] = useState(
-    users.map(u => new UserEntry(u, false))
-  );
 
+  const menuVisible = hasAnyPrivilege(privilegeContext.systemPrivileges, new Set([
+    ...privilegeOthers.assign,
+    ...privilegeOthers.revoke
+  ]))
+
+  // set context menu position. not using transform(-100%, 0%) to handle content bounds
+  // NOTE: COPIED FROM ROLE LIST PAGE
+  // TODO: handle when context menu out of content bounds
   useEffect(() => {
-    setUserEntries(users.map(u => new UserEntry(u, false)));
-  }, [users]);
+    if (cmRef.current) {
+      const current = cmRef.current as any;
+      current.style.left = `${cmData.clientX - (cmRef.current as any).offsetWidth}px`;
+      current.style.top = `${cmData.clientY}px`;
+    }
+  })
 
-  const _renderedUserEntries: any[] = userEntries.map(u => {
+  // close context menu when clicking outside
+  useEffect(() => {
+    const handler = (e: any) => {
+      if (cmRef.current) {
+        if (cmData.visible && !(cmRef.current as any).contains(e.target)) {
+          _closeContextMenu();
+        }
+      }
+    }
+    document.addEventListener('click', handler);
+    return () => {
+      document.removeEventListener('click', handler);
+    }
+  }, [cmData, cmRef]);
+
+  //todo fix assign dialog closing incorrectly
+
+  // close dialog when clicking outside
+  // useEffect(() => {
+  //   const handler = (e: any) => {
+  //     if (dialogRef.current) {
+  //       if (dialogData.visible && !(dialogRef.current as any).contains(e.target)) {
+  //         _closeDialog();
+  //       }
+  //     }
+  //   }
+  //   document.addEventListener('click', handler);
+  //   return () => {
+  //     document.removeEventListener('click', handler);
+  //   }
+  // }, [dialogData, dialogRef]);
+
+  // fill users on startup
+  useEffect(() => {
+    _fetchUsers()
+  }, [])
+
+  function _fetchUsers() {
+    api.withReauth(() => api.profile.getAllUsers())
+      .then(r => {
+        const l = r.data.map(user => toUserModel(user))
+        setUsers(l);
+      })
+  }
+
+  const _renderedUserEntries: any[] = users.map(u => {
     return new PageEntry(() => { return _renderUserEntry(u) });
   });
 
-  function _renderUserEntry(ue: UserEntry) {
+  function _renderUserEntry(ue: UserModel) {
     return (
-      <div className={styles.user_entry} key={ue.data.id}>
-        <div className={styles.user_info}>
-          <span className={styles.user_text}>
-            {ue.data.name} {ue.data.surname}
-          </span>
-          <span className={styles.user_email}>
-            {ue.data.email}
-          </span>
-          <div>
-            {ue.expanded && (
-              <div className={styles.user_table}>
-                <table>
-                  <thead>
-                  <tr>
-                    <th>Роль</th>
-                    <th>Описание</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {ue.data.roles.map(role => (
-                    <tr key={role.name}>
-                      <td>{role.name}</td>
-                      <td>{role.description}</td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </table>
+      <div key={uid()} className={styles.user}>
+        <div className={styles.user_entry}>
+          <div className={styles.user_left}>
+            <div className={styles.user_heading}>
+              <div className={styles.user_name}>
+                {ue.name} {ue.surname}
               </div>
-            )}
+              <div className={styles.user_role}>
+                {ue.role}
+              </div>
+            </div>
+            <div className={styles.user_login}>
+              {ue.loginType}: {ue.login}
+            </div>
           </div>
-        </div>
-        <div className={styles.entry_side_container}>
-          <div className={styles.read_button_container}>
-            <Button onClick={() => setDialogData(new DialogData('Назначение ролей', true, { roles: ue.data.roles }))}>
-              Назначить роли
-            </Button>
-          </div>
-          <div className={styles.arrow_container}>
-            <ArrowDown
-              onClick={() => _expandEntryClick(ue)}
-              className={ue.expanded ? styles.arrow : appendClassName(styles.arrow, styles.arrow_up)}
-            />
+          <div className={styles.user_right}>
+            <div className={styles.read_button_container}>
+              {menuVisible
+                ? <MenuVertical onClick={(e) => _onMenuClick(ue, e)} className={styles.icon_dots} />
+                : <div></div>}
+            </div>
           </div>
         </div>
       </div>
     );
   }
 
-  function _expandEntryClick(userEntry: UserEntry) {
-    setUserEntries(userEntries.map(ue => {
-      if (ue.data.id === userEntry.data.id) {
-        ue.expanded = !ue.expanded;
-      }
-      return ue;
-    }));
+  const _assignRoleToUser = (userId: number, roleId: number) => {
+    //todo rewrite alerts to error messages and success operations
+    api.withReauth(() => api.role.assignSystemRole(userId, roleId))
+      .then(_ => {
+        _fetchUsers()
+        setDialogData(new DialogData('Операция прошла успешно!',
+          DialogSelected.MESSAGE,
+          { messageText: 'Роль пользователя отозвана.' }));
+      }).catch(error => {
+      setDialogData(new DialogData('Некорректная операция!',
+        DialogSelected.MESSAGE,
+        { messageText: error.response.data }));
+    })
+  }
+
+  const _revokeRoleFromUser = (userId: number) => {
+    //todo rewrite alerts to error messages and success operations
+    api.withReauth(() => api.role.revokeSystemRole(userId))
+      .then(_ => {
+        _fetchUsers()
+        setDialogData(new DialogData('Операция прошла успешно!',
+          DialogSelected.MESSAGE,
+          { messageText: 'Роль пользователя отозвана.' }));
+      }).catch(error => {
+        //todo throw error message without error code
+      setDialogData(new DialogData('Некорректная операция!',
+        DialogSelected.MESSAGE,
+        { messageText: error.response.data }));
+    })
   }
 
   const _onSearch = () => {
@@ -173,23 +219,79 @@ export default function UserListPage() {
     setDialogData(new DialogData());
   }
 
+  const _closeContextMenu = () => {
+    setCmData(new ContextMenuData());
+  }
+
   const _Dialog = () => {
     let component = <></>
-    component = <AssignDialogContent
-      onDone={() => console.log("assign roles")}
-      roles={_userRoles}
-      {...dialogData.args}
-    />
+    switch (dialogData.visible) {
+      case DialogSelected.ASSIGN:
+        component = <AssignDialogContent
+          onDone={_assignRoleToUser}
+          {...dialogData.args}
+        />
+        break;
+      case DialogSelected.REVOKE:
+        component = <RevokeDialogContent
+          onDone={_revokeRoleFromUser}
+          {...dialogData.args}
+        />;
+        break;
+      case DialogSelected.MESSAGE:
+        component = <MessageDialogContent
+          onDone={_closeDialog}
+          {...dialogData.args}
+        />;
+        break;
+    }
     return (
       <Dialog
         className={appendClassName(styles.dialog,
           (dialogData.visible ? styles.visible : styles.hidden))}
         text={dialogData.heading}
+        //todo solve unclosable assign dialog issue
+        // ref={dialogRef}
         onClose={_closeDialog}
       >
         {component}
       </Dialog>
     )
+  }
+
+  const _ContextMenu = () => {
+    return <ContextMenu
+      items={cmData.items}
+      className={appendClassName(styles.context_menu,
+        (cmData.visible ? styles.visible : styles.hidden))}
+      ref={cmRef}
+    />;
+  }
+
+  const _onMenuClick = (user: UserModel, e: React.MouseEvent) => {
+    const contextItems: ContextMenuItem[] = [
+      new ContextMenuItem('Назначить роль', (e: React.MouseEvent) => {
+        setCmData({ ...cmData, visible: false });
+        setDialogData(new DialogData('Назначить роль', DialogSelected.ASSIGN, { userId: user.id }));
+        e.stopPropagation();
+      }),
+      new ContextMenuItem('Отозвать роль', () => {
+        setCmData({ ...cmData, visible: false });
+        setDialogData(new DialogData('Отозвать роль', DialogSelected.REVOKE, { userId: user.id }));
+        e.stopPropagation();
+      }),
+    ]
+
+    const mine = privilegeContext.systemPrivileges;
+
+    const filteredItems = contextItems.filter(item => {
+      switch (item.text) {
+        case 'Назначить роль': return hasAnyPrivilege(mine, privilegeOthers.assign)
+        case 'Отозвать роль': return hasAnyPrivilege(mine, privilegeOthers.revoke)
+      }
+    })
+    e.stopPropagation();
+    setCmData(new ContextMenuData(e.clientX, e.clientY, true, filteredItems));
   }
 
   return (
@@ -203,6 +305,7 @@ export default function UserListPage() {
             <Search onSearch={_onSearch} placeholder="Поиск" />
           </div>
           <PagedList page={1} page_size={5} page_step={5} items={_renderedUserEntries} />
+          <_ContextMenu/>
           <Fade
             className={appendClassName(styles.fade,
               (dialogData.visible) ? styles.visible : styles.hidden)}>
