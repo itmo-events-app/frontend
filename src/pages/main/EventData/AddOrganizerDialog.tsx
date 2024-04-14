c08258cef825736c459a2e9838ae7c7b87fad65c
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-14 17:36:46 +0300

    hotfix: api update

--- src/pages/main/EventData/AddOrganizerDialog.tsx
+++ src/pages/main/EventData/AddOrganizerDialog.tsx
@@ -18,10 +18,13 @@ const AddOrganizerDialog = ({ eventId, onSubmit }: { eventId: number; onSubmit: 
   const initDialog = async () => {
     const getUsersResponse = await api.profile.getAllUsers();
     const getAllRoles = await api.role.getAllRoles();
-    if (getUsersResponse.status == 200 && getAllRoles.status==200) {
-      setUserList(getUsersResponse.data);
-      setUserId(getUsersResponse.data[0].id)
-      const eventRoles = getAllRoles.data.filter(r=>r.type=="EVENT");
+    if (getUsersResponse.status == 200 && getAllRoles.status == 200) {
+      const items = getUsersResponse.data.items;
+      setUserList(items ?? []);
+      if (items != null && items.length > 0) {
+        setUserId(items[0].id)
+      }
+      const eventRoles = getAllRoles.data.filter(r => r.type == "EVENT");
       setRoleList(eventRoles);
       setRoleId(eventRoles[0].id);
       setRoleName(eventRoles[0].name);
@@ -57,7 +60,7 @@ const AddOrganizerDialog = ({ eventId, onSubmit }: { eventId: number; onSubmit: 
     <div className={styles.dialog_content}>
       <div className={styles.dialog_item}>
         <InputLabel value="Пользователь" />
-a       <select value={userId} onChange={(e) => setUserId(parseInt(e.target.value))}>
+        a       <select value={userId} onChange={(e) => setUserId(parseInt(e.target.value))}>
           {loaded ? (
             userList.map((u) => {
               return <option value={u.id}>{u.name}</option>;
