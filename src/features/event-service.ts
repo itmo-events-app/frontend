e6ea368456a7c56f2ec92acc541d8cfe321518a5
smugikity <7upnamk@gmail.com>
2024-04-18 07:12:42 +0300

    Data list + fix  (#96)
    * event-list-page: fix css, now call api through eventservice
    
    * fix eslint
    
    * task
    
    * task filter + paging (client side) + fix

--- src/features/event-service.ts
+++ src/features/event-service.ts
@@ -1,21 +1,38 @@
-import { Api } from "@entities/api.ts";
-import { CreateEventRequest } from "@shared/api/generated";
-import { DropdownOption } from "@widgets/main/Dropdown";
+import { Api } from '@entities/api.ts';
+import {
+  CreateEventRequest,
+  GetAllOrFilteredEventsFormatEnum,
+  GetAllOrFilteredEventsStatusEnum,
+} from '@shared/api/generated';
 
 export const eventService = {
-  createTask: (api: Api, name: string, userId: number) => {
+  createEvent: (api: Api, name: string, userId: number) => {
     const request: CreateEventRequest = {
       title: name,
       userId: userId,
     };
-    return api.withReauth(() => api.event.addEventByOrganizer(request));
+    return api.withReauth(() => api.event.addEventByOrganizer(request)).then((response) => response.status === 201);
   },
 
   getUsers: (api: Api) => {
-    return async () => {
-      const response = await api
-        .withReauth(() => api.profile.getAllUsers());
-      return response.data.items!.map((e) => new DropdownOption<string>(`${e.name} ${e.surname} (${e.login})`, e!.id!.toString()));
-    };
+    return api.withReauth(() => api.profile.getAllUsers()).then((response) => response.data);
+  },
+
+  getFilteredEvents: (
+    api: Api,
+    page?: number,
+    size?: number,
+    parentId?: number,
+    title?: string,
+    startDate?: string,
+    endDate?: string,
+    status?: GetAllOrFilteredEventsStatusEnum,
+    format?: GetAllOrFilteredEventsFormatEnum
+  ) => {
+    return api
+      .withReauth(() =>
+        api.event.getAllOrFilteredEvents(page, size, parentId, title, startDate, endDate, status, format)
+      )
+      .then((response) => response.data);
   },
 };
